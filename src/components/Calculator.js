bb4cfbeaf8ef68dfed905bddfad66c89ffe3c2af
Zuhaib Amjad <umar0423@gmail.com>
2022-10-26 20:13:43 +0500

    Implement calculation functionality in project

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,71 +1,161 @@
 import React, { Component } from 'react';
-import Result from './Result';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
+  handleClick = (event) => {
+    const result = calculate(this.state, event.target.innerText);
+    this.setState(result);
+  };
+
   render() {
+    const { total, next, operation } = this.state;
     return (
-      <div className="w-96 mx-auto divide-x divide-y divide-zinc-400 mt-12 grid grid-cols-4 rounded border border-gray-300">
-        <Result />
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+      <div className="w-96 mx-auto divide-x divide-y divide-zinc-400 mt-12 md:flex grid grid-cols-4 rounded border border-gray-300">
+        <div className="result col-span-4 py-4 bg-[#858693] text-2xl text-white text-right pr-2">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           AC
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           +/-
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           %
         </button>
-        <button type="button" className="py-4 bg-[#f4913e]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#f4913e]"
+        >
           ÷
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           7
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           8
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           9
         </button>
-        <button type="button" className="py-4 bg-[#f4913e]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#f4913e]"
+        >
           x
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           4
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           5
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           6
         </button>
-        <button type="button" className="py-4 bg-[#f4913e]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#f4913e]"
+        >
           -
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           1
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           2
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           3
         </button>
-        <button type="button" className="py-4 bg-[#f4913e]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#f4913e]"
+        >
           +
         </button>
-        <button type="button" className="py-4 col-span-2 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 col-span-2 bg-[#e0e0e0]"
+        >
           0
         </button>
-        <button type="button" className="py-4 bg-[#e0e0e0]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#e0e0e0]"
+        >
           .
         </button>
-        <button type="button" className="py-4 bg-[#f4913e]">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="py-4 bg-[#f4913e]"
+        >
           =
         </button>
       </div>
