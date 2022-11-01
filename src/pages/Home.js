badc180f89adf0b12aa475705c66b50a64b2ced5
Zuhaib Amjad <umar0423@gmail.com>
2022-11-02 03:20:14 +0500

    Finish styling Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <h1>Welcome to our Page!</h1>
-      <p>
+      <h1 className="text-4xl text-white font-semibold my-12 px-2 sm:ml-6">
+        Welcome to our Page!
+      </h1>
+      <p className="text-white leading-5 px-2 sm:mx-6">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, illo.
         Aspernatur amet nesciunt maxime esse ratione, voluptatibus molestiae
         iusto assumenda dolores sed labore, eligendi eaque earum reprehenderit,
@@ -18,14 +20,9 @@ export default function Home() {
         nesciunt maxime esse ratione, voluptatibus molestiae iusto assumenda
         dolores sed labore, eligendi eaque earum reprehenderit, pariatur iste
         incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad,
-        illo. Aspernatur amet nesciunt maxime esse ratione, voluptatibus
-        molestiae iusto assumenda dolores sed labore, eligendi eaque earum
-        reprehenderit, pariatur iste incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Ad, illo. Aspernatur amet nesciunt maxime
-        esse ratione, voluptatibus molestiae iusto assumenda dolores sed labore,
-        eligendi eaque earum reprehenderit, pariatur iste incidunt.
+        illo.
       </p>
-      <p>
+      <p className="text-white leading-5 px-2 sm:mx-6">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, illo.
         Aspernatur amet nesciunt maxime esse ratione, voluptatibus molestiae
         iusto assumenda dolores sed labore, eligendi eaque earum reprehenderit,
@@ -37,12 +34,7 @@ export default function Home() {
         nesciunt maxime esse ratione, voluptatibus molestiae iusto assumenda
         dolores sed labore, eligendi eaque earum reprehenderit, pariatur iste
         incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad,
-        illo. Aspernatur amet nesciunt maxime esse ratione, voluptatibus
-        molestiae iusto assumenda dolores sed labore, eligendi eaque earum
-        reprehenderit, pariatur iste incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Ad, illo. Aspernatur amet nesciunt maxime
-        esse ratione, voluptatibus molestiae iusto assumenda dolores sed labore,
-        eligendi eaque earum reprehenderit, pariatur iste incidunt.
+        illo.
       </p>
     </div>
   );
